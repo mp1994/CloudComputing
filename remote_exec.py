5d24f6a590e834e53b5d74c76b66b1eb7da02189
Mattia <mattia.pesenti@polimi.it>
2022-02-22 23:12:52 +0100

    Try remote_exec() with NamedTemporaryFile

--- remote_exec.py
+++ remote_exec.py
@@ -2,24 +2,19 @@ import os
 import subprocess
 import tempfile as tf
 from vars import ssh_port, ssh_host
-DEBUG = False
 
 def remote_exec(path):
     print("Exec from: {}".format(path))
-    # Open calling script (from path) and read the file
+    # Open the calling script (from path) and read the file
     fin = open(path, 'r')
     # Split the script and take everything after separator
     s = fin.read().split("remote_exec(__file__)")[-1]
-    # DEBUG: print script portion
-    if DEBUG:
-        print(s)
     # Write to file
-    tmp = "foo.py" #tf.NamedTemporaryFile(suffix=".py")
-    fout = open(tmp, 'w')
-    print(fout.name)
+    tmp = tf.NamedTemporaryFile(suffix=".py")
+    fout = open(tmp.name, 'w')
     fout.write(s)
     fout.close()
     # Run over SSH
-    subprocess.Popen("/usr/bin/ssh -p {} {} python3 -u - < {}".format(ssh_port, ssh_host, fout.name), shell=True)
-    # Exit to prevent calling script to run locally
+    subprocess.Popen("/usr/bin/ssh -p {} {} python3 -u - < {}".format(ssh_port, ssh_host, tmp.name), shell=True)
+    # Exit to prevent the calling script to run locally
     exit(0)
