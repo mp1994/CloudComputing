b0c84ca2ad0856b5cae8b6e426c971c736dce97b
Mattia <mattia.pesenti@polimi.it>
2022-02-22 22:07:04 +0100

    Improved config(); added config_ssh()

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import cloudsync as cs
 import json
 import os
 from os.path import exists
+import subprocess
 
 def get_auth_token():
     oauth_config = cs.command.utils.generic_oauth_config('onedrive')
@@ -17,14 +18,20 @@ def check_config(silent=False):
     defaultPath = os.environ['HOME'] + "/.creds.json"
     if exists(defaultPath):
         if not silent:
-            print("Configuration found in: {}".format(defaultPath))
+            print("[INFO] Configuration found in: {}".format(defaultPath))
         return defaultPath
     else:
         if not silent:
-            print("No configuration found in default path.")
+            print("[INFO] No configuration found in default path.")
+        return False
 
-def config():
-    print("Starting OneDrive auth...")
+def config(Force=False):
+    if check_config() == False:
+        print("Starting OneDrive auth...")
+    else:
+        if Force == False:
+            print("Use check_config(Force=True) to overwrite existing configuration.")
+            return
     token = get_auth_token()
     defaultPath = os.environ['HOME'] + "/"
     print("Specify path to save OneDrive auth token ({}): ".format(defaultPath), end='')
@@ -33,4 +40,18 @@ def config():
         f = defaultPath
     f = f + ".creds.json"
     save_auth_token(f, creds=token)
-    print("Token saved to file: {}".format(f))
+    print("[INFO] Token saved to file: {}".format(f))
+
+def config_ssh(host=None, port=22):
+    if host is None:
+        print("[ERROR] The first input cannot be None: provide a username-host pair, such as: user@127.0.0.1")
+        return
+    cmd = "ssh -p {} {} uname -n".format(port, host)
+    print("Testing connection: " + cmd)
+    cmd = "ssh -p {} {} uname -n".format(port, host)
+    try:
+        out = subprocess.check_output(cmd, shell=True).decode().strip()
+    except subprocess.CalledProcessError:
+        print("[WARNING] SSH connection issue.")
+        return
+    print("[INFO] Connected to host: {}".format(out))
