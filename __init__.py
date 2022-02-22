8d185a93dd8332a91e609289f7d91b9f1f6f56f2
Mattia <mattia.pesenti@polimi.it>
2022-02-22 23:31:28 +0100

    global variable sharing works!

--- __init__.py
+++ __init__.py
@@ -2,16 +2,16 @@ from cloud_storage import connect, change_namespace, download_file, upload_file
 from config import config, check_config, check_ssh_connection
 from remote_exec import remote_exec
 import configparser
-from vars import *
+import vars
 
 # CloudComputing version
 _version = "0.0.2"
 
 ## Global variables
 # cloud_storage
-creds_path = check_config(silent=True)
+vars.creds_path = check_config(silent=True)
 # remote_ssh
 c = configparser.ConfigParser()
 c.read("config.ini")
-ssh_host = c['SSH']['host']
-ssh_port = c['SSH']['port']
+vars.ssh_host = c['SSH']['host']
+vars.ssh_port = c['SSH']['port']
--- remote_exec.py
+++ remote_exec.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 import tempfile as tf
-from vars import ssh_port, ssh_host
+import vars
 
 def remote_exec(path):
     print("Exec from: {}".format(path))
@@ -10,11 +10,12 @@ def remote_exec(path):
     # Split the script and take everything after separator
     s = fin.read().split("remote_exec(__file__)")[-1]
     # Write to file
-    tmp = tf.NamedTemporaryFile(suffix=".py")
-    fout = open(tmp.name, 'w')
+    tmp = os.path.join(tf.gettempdir(), os.urandom(8).hex() + '.py')
+    fout = open(tmp, 'w')
     fout.write(s)
     fout.close()
     # Run over SSH
-    subprocess.Popen("/usr/bin/ssh -p {} {} python3 -u - < {}".format(ssh_port, ssh_host, tmp.name), shell=True)
+    print(vars.ssh_host + " " + vars.ssh_port)
+    subprocess.Popen("/usr/bin/ssh -p {} {} python3 -u - < {}".format(vars.ssh_port, vars.ssh_host, tmp), shell=True)
     # Exit to prevent the calling script to run locally
     exit(0)
