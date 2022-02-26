35dd35749bad3556ad14fa6ebe069a4900930843
Mattia <mattia.pesenti@polimi.it>
2022-02-26 12:14:32 +0100

    Exit if locahost; added verbose and logfile control

--- remote_exec.py
+++ remote_exec.py
@@ -3,7 +3,11 @@ import subprocess
 import tempfile as tf
 import vars
 
-def remote_exec(path):
+def remote_exec(path, verbose=True, logfile=None):
+    # If localhost, return
+    if vars.ssh_host == 'localhost' or vars.ssh_host == '127.0.0.1':
+        print("Running on local machine...")
+        return
     print("Exec from: {}".format(path))
     # Open the calling script (from path) and read the file
     fin = open(path, 'r')
@@ -16,6 +20,12 @@ def remote_exec(path):
     fout.close()
     # Run over SSH
     print(vars.ssh_host + " " + vars.ssh_port)
-    subprocess.Popen("/usr/bin/ssh -p {} {} python3 -u - < {}".format(vars.ssh_port, vars.ssh_host, tmp), shell=True)
+    cmd = "/usr/bin/ssh -p {} {} python3 -u - < {}".format(vars.ssh_port, vars.ssh_host, tmp)
+    if not verbose:
+        cmd = cmd + " 1>/dev/null 2>&1"
+    if not logfile is None:
+        print("Logging to file: {}".format(logfile))
+        cmd = cmd + " > {}".format(logfile)
+    subprocess.Popen(cmd, shell=True)
     # Exit to prevent the calling script to run locally
     exit(0)
