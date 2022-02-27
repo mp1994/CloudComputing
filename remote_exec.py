be575e2e4f7f5b8e5ce5ab8fd8f2d4681bcc1180
Mattia <mattia.pesenti@polimi.it>
2022-02-27 16:49:37 +0100

    Added option to 'cd' into a remote folder (this allows to exploits local python versions with 'pyenv'

--- remote_exec.py
+++ remote_exec.py
@@ -3,7 +3,7 @@ import subprocess
 import tempfile as tf
 import vars
 
-def remote_exec(path, verbose=True, logfile=None):
+def remote_exec(path, rdir="", verbose=True, logfile=None):
     # If localhost, return
     if 'localhost' in vars.ssh_host or '127.0.0.1' in vars.ssh_host:
         print("Running on local machine...")
@@ -13,14 +13,21 @@ def remote_exec(path, verbose=True, logfile=None):
     fin = open(path, 'r')
     # Split the script and take everything after separator
     s = fin.read().split("remote_exec(__file__)")[-1]
+    # Do we need to import CloudComputing? 
+    if "CloudComputing" in s or "cc" in s:
+        s = "import CloudComputing as cc\n" + s
     # Write to file
     tmp = os.path.join(tf.gettempdir(), os.urandom(8).hex() + '.py')
     fout = open(tmp, 'w')
     fout.write(s)
     fout.close()
+    # CD to remote dir (if any)
+    cmd = cmd = "/usr/bin/ssh -p {} {}".format(vars.ssh_port, vars.ssh_host)
+    cmd = cmd + " 'cd {} &&".format(rdir)
     # Run over SSH
     print(vars.ssh_host + " " + vars.ssh_port)
-    cmd = "/usr/bin/ssh -p {} {} python3 -u - < {}".format(vars.ssh_port, vars.ssh_host, tmp)
+    cmd = cmd + " python -u - < {}'".format(tmp)
+    print(cmd)
     if not verbose:
         cmd = cmd + " 1>/dev/null 2>&1"
     if not logfile is None:
