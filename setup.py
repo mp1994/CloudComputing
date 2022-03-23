6b0d1b278d42307e872955021f5fdf2219a16d57
Mattia <mattia.pesenti@polimi.it>
2022-03-23 22:34:26 +0100

    Version 0.1.2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.1' 
+VERSION = '0.1.2' 
 DESCRIPTION = 'CloudComputing package'
 LONG_DESCRIPTION = 'The CloudComputing package can be used to ease remote executing over SSH and cloud storage (OneDrive) with Python.'
 
